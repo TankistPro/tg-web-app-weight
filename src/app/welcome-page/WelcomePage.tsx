05964abee649ef7f5293bf67b6ec08b42185d364
TankistPro <nakol.nayk@mail.ru>
2024-10-06 13:53:03 +0300

    base structure

---
+++ src/app/welcome-page/WelcomePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WelcomePage = () => {
+    return <div>WelcomPage</div>;
+};
+
+export default WelcomePage;
---
+++ src/app/welcome-page/page.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+import WelcomePage from './WelcomePage';
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+    return <WelcomePage />;
+};
+
+export default Page;
